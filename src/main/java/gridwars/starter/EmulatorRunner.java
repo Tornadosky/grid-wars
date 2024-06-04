a503277de184cd4623e0997e69a7cd3c83255726
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2024-06-04 10:02:55 +0200

    Add simple bots

---
+++ src/main/java/gridwars/starter/EmulatorRunner.java
@@ -0,0 +1,12 @@
+package gridwars.starter;
+
+import cern.ais.gridwars.Emulator;
+
+/**
+ * Instantiates the example bots and starts the game emulator.
+ */
+public class EmulatorRunner {
+    public static void main(String[] args) {
+        Emulator.playMatch(new ExpandBot(), new ExpandBot());
+    }
+}
